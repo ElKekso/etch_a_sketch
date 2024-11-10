5abf9b357265bea15151aad6d3603ac639e3b5c6
ElKekso <159456154+ElKekso@users.noreply.github.com>
2024-11-10 19:17:35 +0100

    Create grid script and hover effect

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const grid = document.querySelector("#grid");
 
+for(let i = 0; i < 16*16; i++)
+{
+    let element = document.createElement("div");
+    element.classList.add("square");
+    grid.appendChild(element);
+}
